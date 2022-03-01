81f1478e18928a17af43873f38d20efaa162c724
Lucas Vieira <lucas.vieira94@outlook.com>
2022-03-01 19:23:59 -0300

    Add @nxlv/python initial version

---
+++ lucasvieira/e2e/nx-python-e2e/tests/nx-python.spec.ts
@@ -0,0 +1,44 @@
+import {
+  checkFilesExist,
+  ensureNxProject,
+  readJson,
+  runNxCommandAsync,
+  uniq,
+} from '@nrwl/nx-plugin/testing';
+describe('nx-python e2e', () => {
+  it('should create nx-python', async () => {
+    const plugin = uniq('nx-python');
+    ensureNxProject('@lucasvieira/nx-python', 'dist/packages/nx-python');
+    await runNxCommandAsync(
+      `generate @lucasvieira/nx-python:nx-python ${plugin}`
+    );
+
+    const result = await runNxCommandAsync(`build ${plugin}`);
+    expect(result.stdout).toContain('Executor ran');
+  }, 120000);
+
+  describe('--directory', () => {
+    it('should create src in the specified directory', async () => {
+      const plugin = uniq('nx-python');
+      ensureNxProject('@lucasvieira/nx-python', 'dist/packages/nx-python');
+      await runNxCommandAsync(
+        `generate @lucasvieira/nx-python:nx-python ${plugin} --directory subdir`
+      );
+      expect(() =>
+        checkFilesExist(`libs/subdir/${plugin}/src/index.ts`)
+      ).not.toThrow();
+    }, 120000);
+  });
+
+  describe('--tags', () => {
+    it('should add tags to the project', async () => {
+      const plugin = uniq('nx-python');
+      ensureNxProject('@lucasvieira/nx-python', 'dist/packages/nx-python');
+      await runNxCommandAsync(
+        `generate @lucasvieira/nx-python:nx-python ${plugin} --tags e2etag,e2ePackage`
+      );
+      const project = readJson(`libs/${plugin}/project.json`);
+      expect(project.tags).toEqual(['e2etag', 'e2ePackage']);
+    }, 120000);
+  });
+});
---
+++ lucasvieira/packages/nx-python/src/generators/nx-python/schema.d.ts
@@ -0,0 +1,5 @@
+export interface NxPythonGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
