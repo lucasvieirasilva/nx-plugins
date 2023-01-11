30c594afaaddad46942c6002bc6c4989a07b983f
Lucas Vieira <lucas.vieira94@outlook.com>
2023-01-11 10:00:20 -0300

    remove index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-var commandExists = require('command-exists');
-
-// invoked without a callback, it returns a promise
-commandExists('ls').then((command) => console.log(`OK: ${command}`)).catch((err) => console.error(`NOT OK: ${err}`));
