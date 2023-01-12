3f63ce33e12a6d8f94750452525093381374ffcd
Lucas Vieira <lucas.vieira94@outlook.com>
2023-01-12 10:47:46 -0300

    chore(workspace): added github issue and PR templates, dependabot config and git add to lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,7 @@ module.exports = {
   '{packages,tools}/**/*.{ts,js,json,md,html,css,scss}': [
     'npx nx affected --target lint --uncommitted --fix true',
     'npx nx format:write --uncommitted',
+    'git add',
   ],
   '*.{js,md,json}': ['npx nx format:write --uncommitted', 'git add'],
 };
