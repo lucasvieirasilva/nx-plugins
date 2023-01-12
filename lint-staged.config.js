99a7b9d2b9c80caa97052bb92221eb2d0bdc4b3f
Lucas Vieira <lucas.vieira94@outlook.com>
2023-01-12 10:18:55 -0300

    chore(nx-python): added semantic-release/github and remove unit test from lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   '{packages,tools}/**/*.{ts,js,json,md,html,css,scss}': [
     'npx nx affected --target lint --uncommitted --fix true',
-    'npx nx affected --target test --uncommitted',
     'npx nx format:write --uncommitted',
+    'git add',
   ],
-  '*.{js,md,json}': ['npx nx format:write --uncommitted'],
+  '*.{js,md,json}': ['npx nx format:write --uncommitted', 'git add'],
 };
