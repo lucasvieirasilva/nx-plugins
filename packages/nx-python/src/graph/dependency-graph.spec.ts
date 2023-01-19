0951c18883e862296cabec2ad071d45fffd54ab3
Niall McAndrew <niall@mediasuite.co.nz>
2023-01-20 09:45:17 +1300

    feat(nx-python): follow all groups with dep graph
    Ensures that the dependency graph follows all arbitrary pip dependency groups.

--- packages/nx-python/src/graph/dependency-graph.spec.ts
+++ packages/nx-python/src/graph/dependency-graph.spec.ts
@@ -118,6 +118,190 @@ describe('nx-python dependency graph', () => {
     });
   });
 
+  it('should link dev dependencies in the graph', async () => {
+    fsMock({
+      'apps/app1/pyproject.toml': dedent`
+      [tool.poetry]
+      name = "app1"
+      version = "1.0.0"
+        [tool.poetry.group.dev.dependencies]
+        python = "^3.8"
+        dep1 = { path = "../../libs/dep1" }
+      `,
+
+      'libs/dep1/pyproject.toml': dedent`
+      [tool.poetry]
+      name = "dep1"
+      version = "1.0.0"
+        [tool.poetry.dependencies]
+        python = "^3.8"
+      `,
+    });
+
+    const mockBuilder = new ProjectGraphBuilder(null);
+
+    mockBuilder.addNode({
+      name: 'app1',
+      type: 'app',
+      data: {
+        root: 'apps/app1',
+        files: [],
+      },
+    });
+
+    mockBuilder.addNode({
+      name: 'dep1',
+      type: 'lib',
+      data: {
+        root: 'libs/dep1',
+        files: [],
+      },
+    });
+
+    const result = processProjectGraph(mockBuilder.graph, {
+      fileMap: {},
+      filesToProcess: {},
+      workspace: {
+        projects: {
+          app1: {
+            root: 'apps/app1',
+            targets: {},
+          },
+          dep1: {
+            root: 'libs/dep1',
+            targets: {},
+          },
+        },
+        version: 2,
+        npmScope: 'test',
+      },
+    });
+
+    expect(result).toStrictEqual({
+      dependencies: {
+        app1: [
+          {
+            source: 'app1',
+            target: 'dep1',
+            type: 'implicit',
+          },
+        ],
+        dep1: [],
+      },
+      externalNodes: {},
+      nodes: {
+        app1: {
+          name: 'app1',
+          type: 'app',
+          data: {
+            root: 'apps/app1',
+            files: [],
+          },
+        },
+        dep1: {
+          name: 'dep1',
+          type: 'lib',
+          data: {
+            root: 'libs/dep1',
+            files: [],
+          },
+        },
+      },
+    });
+  });
+
+  it('should link arbitrary groups dependencies in the graph', async () => {
+    fsMock({
+      'apps/app1/pyproject.toml': dedent`
+      [tool.poetry]
+      name = "app1"
+      version = "1.0.0"
+        [tool.poetry.group.example_group.dependencies]
+        python = "^3.8"
+        dep1 = { path = "../../libs/dep1" }
+      `,
+
+      'libs/dep1/pyproject.toml': dedent`
+      [tool.poetry]
+      name = "dep1"
+      version = "1.0.0"
+        [tool.poetry.dependencies]
+        python = "^3.8"
+      `,
+    });
+
+    const mockBuilder = new ProjectGraphBuilder(null);
+
+    mockBuilder.addNode({
+      name: 'app1',
+      type: 'app',
+      data: {
+        root: 'apps/app1',
+        files: [],
+      },
+    });
+
+    mockBuilder.addNode({
+      name: 'dep1',
+      type: 'lib',
+      data: {
+        root: 'libs/dep1',
+        files: [],
+      },
+    });
+
+    const result = processProjectGraph(mockBuilder.graph, {
+      fileMap: {},
+      filesToProcess: {},
+      workspace: {
+        projects: {
+          app1: {
+            root: 'apps/app1',
+            targets: {},
+          },
+          dep1: {
+            root: 'libs/dep1',
+            targets: {},
+          },
+        },
+        version: 2,
+        npmScope: 'test',
+      },
+    });
+
+    expect(result).toStrictEqual({
+      dependencies: {
+        app1: [
+          {
+            source: 'app1',
+            target: 'dep1',
+            type: 'implicit',
+          },
+        ],
+        dep1: [],
+      },
+      externalNodes: {},
+      nodes: {
+        app1: {
+          name: 'app1',
+          type: 'app',
+          data: {
+            root: 'apps/app1',
+            files: [],
+          },
+        },
+        dep1: {
+          name: 'dep1',
+          type: 'lib',
+          data: {
+            root: 'libs/dep1',
+            files: [],
+          },
+        },
+      },
+    });
+  });
+
   it('should progress the dependency graph for an empty project', async () => {
     const result = processProjectGraph(null, {
       fileMap: {},
--- packages/nx-python/src/graph/dependency-graph.ts
+++ packages/nx-python/src/graph/dependency-graph.ts
@@ -28,11 +28,9 @@ export type PyprojectTomlDependencies = {
   [key: string]: PyprojectTomlDependency;
 };
 
-export type DependencyCategory = 'main' | 'dev';
-
 export type Dependency = {
   name: string;
-  category: DependencyCategory;
+  category: string;
 };
 
 export type PyprojectToml = {
@@ -42,6 +40,7 @@ export type PyprojectToml = {
       packages: Array<{
         include: string;
       }>;
+      version: string;
       dependencies: PyprojectTomlDependencies;
       group?: {
         [key: string]: {
@@ -90,6 +89,7 @@ export const getDependencies = (
 
   if (existsSync(pyprojectToml)) {
     const tomlData = getPyprojectData(pyprojectToml);
+
     resolveDependencies(
       tomlData.tool.poetry.dependencies,
       projectData,
@@ -98,14 +98,16 @@ export const getDependencies = (
       deps,
       'main'
     );
-    resolveDependencies(
-      tomlData.tool.poetry.group?.dev.dependencies,
-      projectData,
-      workspace,
-      cwd,
-      deps,
-      'dev'
-    );
+    for (const group in tomlData.tool.poetry.group) {
+      resolveDependencies(
+        tomlData.tool.poetry.group[group].dependencies,
+        projectData,
+        workspace,
+        cwd,
+        deps,
+        group
+      );
+    }
   }
 
   return deps;
@@ -142,20 +144,25 @@ const checkProjectIsDependent = (
   if (existsSync(pyprojectToml)) {
     const tomlData = getPyprojectData(pyprojectToml);
 
-    return (
-      isProjectDependent(
-        tomlData.tool.poetry.dependencies,
-        projectData,
-        root,
-        cwd
-      ) ||
-      isProjectDependent(
-        tomlData.tool.poetry.group?.dev.dependencies,
-        projectData,
-        root,
-        cwd
-      )
+    let isDep = isProjectDependent(
+      tomlData.tool.poetry.dependencies,
+      projectData,
+      root,
+      cwd
     );
+
+    if (isDep) return true;
+
+    for (const group in tomlData.tool.poetry.group) {
+      isDep = isProjectDependent(
+        tomlData.tool.poetry.group[group].dependencies,
+        projectData,
+        root,
+        cwd
+      );
+
+      if (isDep) return true;
+    }
   }
 
   return false;
@@ -189,7 +196,7 @@ const resolveDependencies = (
   workspace: Workspace | ProjectsConfigurations,
   cwd: string,
   deps: Dependency[],
-  category: DependencyCategory
+  category: string
 ) => {
   for (const dep in dependencies || {}) {
     const depData = dependencies[dep];
