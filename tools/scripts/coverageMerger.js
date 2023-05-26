e1afca3926537d4ff1f2bb2fc24b430a44df5af4
Lucas Vieira <lucas.vieira94@outlook.com>
2023-05-26 09:06:50 -0300

    chore(workspace): nx format

--- tools/scripts/coverageMerger.js
+++ tools/scripts/coverageMerger.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const getLcovFiles = function (src) {
-  return globSync(`${src}/**/lcov.info`)
+  return globSync(`${src}/**/lcov.info`);
 };
 
 (async function () {
