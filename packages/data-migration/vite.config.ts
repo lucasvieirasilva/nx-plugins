519446e9e3e08ebea288e915041e0ea94361b0e5
Lucas Vieira <lucas.vieira94@outlook.com>
2024-03-06 09:14:13 -0300

    chore: set vitest pool config and rollback github actions changes

--- packages/data-migration/vite.config.ts
+++ packages/data-migration/vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 
     reporters: ['default'],
+    pool: 'threads',
     coverage: {
       enabled: true,
       reporter: [
