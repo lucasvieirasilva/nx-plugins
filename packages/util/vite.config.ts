54c34273c471510e534a73062528b2e40da7202c
Lucas Vieira <lucas.vieira94@outlook.com>
2024-03-06 09:35:44 -0300

    chore: rollback vitest pool config and downgrade vite package

--- packages/util/vite.config.ts
+++ packages/util/vite.config.ts
@@ -24,7 +24,6 @@ export default defineConfig({
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 
     reporters: ['default'],
-    pool: 'threads',
     coverage: {
       enabled: true,
       reporter: [
