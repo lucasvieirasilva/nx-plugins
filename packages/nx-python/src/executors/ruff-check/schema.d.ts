9ac8462a393bc0c762fe32c899b04acb8886329f
🚀 Jack <jacsteve@amazon.com>
2026-02-12 13:22:18 +1100

    feat(nx-python): add exitZero option to ruff-check executor
    Allow users to skip failures due to lint issues by passing the
    `--exit-zero` flag to `ruff check`
    
    Fixes #343

--- packages/nx-python/src/executors/ruff-check/schema.d.ts
+++ packages/nx-python/src/executors/ruff-check/schema.d.ts
@@ -3,5 +3,6 @@ import { BaseExecutorSchema } from '../base-schema';
 export interface RuffCheckExecutorSchema extends BaseExecutorSchema {
   lintFilePatterns: string[];
   fix?: boolean;
+  exitZero?: boolean;
   __unparsed__: string[];
 }
